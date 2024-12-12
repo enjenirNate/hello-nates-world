7e961fea97ab47cbe6f02f3569856020cc63ed12
enjenirNate <nathaniel.leung@stemcell.com>
2024-12-11 17:03:36 -0800

    Added user input

--- natesHello.cpp
+++ natesHello.cpp
@@ -1,11 +1,20 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
+    
+    std::string myName;
     std::cout << "Hello World! This is nates hello world" << std::endl;
     std::cout << "I made a basic change " << std::endl;
     std::cout << "I made another change, playing with git. I wanna go home :(" << std::endl;
+    std::cin >> myName;
+
+    std::cout << "hello " << myName << endl;
+    std::cout << "Goodbye for now " ; 
+     
 
 
     
